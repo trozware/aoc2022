79ca5ab2b2c58e9d41b3367b24777daca431e337
Sarah Reichelt <sarah@troz.net>
2022-12-02 10:05:31 +1000

    day 1

--- AoC2022/days/day01.swift
+++ AoC2022/days/day01.swift
@@ -3,7 +3,9 @@
 import Foundation
 
 func day01(testData: [String], realData: [String]) {
-  let expectedTestResults = [1, 2]
+  // let expectedTestResults = 24000   // Part 1
+  let expectedTestResults = 45000   // Part 2
+
   let testResults = runCode(data: testData)
 
   if testResults != expectedTestResults {
@@ -19,8 +21,29 @@ func day01(testData: [String], realData: [String]) {
   let realResults = runCode(data: realData)
   print("Results:", realResults)
 
-  func runCode(data: [String]) -> [Int] {
+  func runCode(data: [String]) -> Int {
+    var elfNumber = 0
+    var caloriesPerElf: [Int] = [0]
 
-    return [1, 2]
+    for line in data {
+      if line.isEmpty {
+        elfNumber += 1
+        caloriesPerElf.append(0)
+      } else {
+        let calories = Int(line) ?? 0
+        caloriesPerElf[elfNumber] += calories
+      }
+    }
+
+    caloriesPerElf.sort()
+    caloriesPerElf.reverse()
+
+    // Part 1
+    // let maxCalories = caloriesPerElf[0]
+    // return maxCalories  // 67016
+
+    // Part 2
+    let topThree = caloriesPerElf[0] + caloriesPerElf[1] + caloriesPerElf[2]
+    return topThree   // 200116
   }
 }
